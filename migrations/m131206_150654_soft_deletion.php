4aee627a8feddfa9b38e080a33e1e1aaa19c196b
Mark <m@rkw.me>
2013-12-11 11:33:00 +0000

    [OE-3856] soft deletion

--- migrations/m131206_150654_soft_deletion.php
+++ migrations/m131206_150654_soft_deletion.php
@@ -4,9 +4,13 @@ class m131206_150654_soft_deletion extends CDbMigration
 {
 	public function up()
 	{
+		$this->addColumn('et_ophleepatientletter_epatientletter','deleted','tinyint(1) unsigned NOT NULL');
+		$this->addColumn('et_ophleepatientletter_epatientletter_version','deleted','tinyint(1) unsigned NOT NULL');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('et_ophleepatientletter_epatientletter','deleted');
+		$this->dropColumn('et_ophleepatientletter_epatientletter_version','deleted');
 	}
 }
