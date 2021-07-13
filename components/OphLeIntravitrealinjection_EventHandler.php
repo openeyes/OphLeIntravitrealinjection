c3386b7f3c588c429d2a29a8d51a7ada463baace
biskyt <biskyt@users.noreply.github.com>
2021-07-13 17:38:45 +0100

    OE-11506 - fix hos_num lookup to work with multi-tenancy

--- components/OphLeIntravitrealinjection_EventHandler.php
+++ components/OphLeIntravitrealinjection_EventHandler.php
@@ -5,12 +5,21 @@ class OphLeIntravitrealinjection_EventHandler
 	public function patientFound($params)
 	{
 		$patient = $params['patient'];
+
+		$institution_id = Institution::model()->getCurrent()->id;
+		$site_id = Yii::app()->session['selected_site_id'];
+		$local_identifier_value = PatientIdentifierHelper::getIdentifierValue(PatientIdentifierHelper::getIdentifierForPatient(
+                    'LOCAL',
+                    $patient->id,
+                    $institution_id, $site_id
+                ));
+
 		// check for any unattached legacy injection events for this patient, and set them up as appropriate
 		if ($elements = Element_OphLeIntravitrealinjection_IntravitrealInjection::model()->findAll(
 				array(
 					'condition' => 'archive_hosnum = :hosnum', 
 					'order' => 'created_date asc', 
-					'params' => array(':hosnum' => $patient->hos_num)
+					'params' => array(':hosnum' => $local_identifier_value)
 				)
 			) ) {
 			foreach ($elements as $el) {
